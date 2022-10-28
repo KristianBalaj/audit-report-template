efcfa0e02a3b096d86c9bde6e79c28d4a7588341
Vlad P. Luchian <cstmlcodes@gmail.com>
2022-10-28 18:04:43 +0100

    update: develop the flake and move build logic into repo

--- flake.nix
+++ flake.nix
@@ -1,12 +1,66 @@
 {
   description = "Audit Report Latex Templates and Macros";
-  outputs = {self, nixpkgs,...}: 
-  {
-    defaultPackage.x86_64-linux = 
-      with import nixpkgs {system = "x86_64-linux";};
-        stdenv.mkDerivation{
-          name = "audit-report-generation";
-          src = ./.;
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system: {
+
+      # template files are included as a dummy package - available to the build
+      # environment
+      template-files = { src = ./.; };
+
+      build-report = with import nixpkgs { inherit system; };
+        { src
+        # ^ source where the latex files can be found
+
+        , file-name ? "audit-report"
+          # ^ root file
+
+        , location ? "."
+          # ^ root folder
+
+        , imported-files ?
+          " $td/style.tex $td/macros.tex  $td/Makefile $td/linked-files "
+          # ^ files that get imported and are available to the environment - these
+          # files also get symlinked in the dev environment
+
+        , td ? self.template-files.${system}.src
+          # ^ template-files dummy package 
+
+        , name ? "template audit report"
+          # ^ name of derivation
+        }:
+        stdenv.mkDerivation {
+          inherit td src name;
+
+          buildInputs = self.defaultPackage.${system}.buildInputs;
+
+          buildPhase = " cp -fr ${imported-files}  .  ";
+
+          installPhase = ''
+            make 
+            mkdir $out
+            cp ${location}/_build/${file-name}.pdf $out/$(date +%y%m%d)-${file-name}.pdf
+          '';
+
+          shellHook = ''
+            echo "> linking template files:"
+            ln -sf ${imported-files} .
+            echo "> Welcome to the audit-report shell."
+          '';
         };
-  };
+
+      defaultPackage = with import nixpkgs { inherit system; };
+        mkShell {
+          name = "audit report generator shell";
+          buildInputs = with nixpkgs;
+            [
+              texlive.combined.scheme-full
+              pandoc
+            ] # packages for latex and file processing
+            ++ [ graphviz zathura entr nixfmt ] # packages for  dev environment
+          ;
+        };
+    });
 }
