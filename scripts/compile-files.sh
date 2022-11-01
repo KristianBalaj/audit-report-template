5a4554a50d9dff85bd0656cd3cdc92eb70a7df71
Vlad P. Luchian <cstmlcodes@gmail.com>
2022-11-01 16:00:20 +0000

    update: change files to .md

--- scripts/compile-files.sh
+++ scripts/compile-files.sh
@@ -1,5 +1,6 @@
 #bin/sh
-markdown-pp $1           \
-	  -o $2              \
-	  -e latexrender
-	pandoc $2 -o $3
+echo "> Pre-processing markdown files."
+markdown-pp $1 -o $2 -e latexrender
+echo "> Running pandoc on the pre-processed result."
+pandoc $2 -o $3
+echo "> Done! Check" $3
